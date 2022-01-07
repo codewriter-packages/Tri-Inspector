a2983055694061b781f59a005d8f0ddf3365dcfb
VladV <vanifatov.vlad@yandex.ru>
2022-01-07 20:21:36 +0300

    Add drawers for unity built-in types

---
+++ Editor.Extras/Drawers/BuiltinDrawerBase.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace TriInspector.Drawers
+{
+    public abstract class BuiltinDrawerBase<T> : TriValueDrawer<T>
+    {
+        public sealed override TriElement CreateElement(TriValue<T> propertyValue, TriElement next)
+        {
+            if (propertyValue.Property.TryGetSerializedProperty(out _))
+            {
+                return next;
+            }
+
+            return base.CreateElement(propertyValue, next);
+        }
+
+        public sealed override float GetHeight(float width, TriValue<T> propertyValue, TriElement next)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        public sealed override void OnGUI(Rect position, TriValue<T> propertyValue, TriElement next)
+        {
+            var value = propertyValue.Value;
+
+            EditorGUI.BeginChangeCheck();
+
+            value = OnValueGUI(position, propertyValue.Property.DisplayNameContent, value);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                propertyValue.Value = value;
+            }
+        }
+
+        protected abstract T OnValueGUI(Rect position, GUIContent label, T value);
+    }
+}
---
+++ Editor.Extras/Drawers/ObjectReferenceDrawer.cs
@@ -0,0 +1,56 @@
+﻿using TriInspector;
+using TriInspector.Drawers;
+using UnityEditor;
+using UnityEngine;
+
+[assembly: RegisterTriValueDrawer(typeof(ObjectReferenceDrawer<>), TriDrawerOrder.Fallback)]
+
+namespace TriInspector.Drawers
+{
+    public class ObjectReferenceDrawer<T> : TriValueDrawer<T>
+        where T : Object
+    {
+        public override TriElement CreateElement(TriValue<T> value, TriElement next)
+        {
+            if (value.Property.TryGetSerializedProperty(out _))
+            {
+                return next;
+            }
+
+            return new ObjectReferenceDrawerElement(value);
+        }
+
+        private class ObjectReferenceDrawerElement : TriElement
+        {
+            private readonly TriValue<T> _propertyValue;
+
+            public ObjectReferenceDrawerElement(TriValue<T> propertyValue)
+            {
+                _propertyValue = propertyValue;
+            }
+
+            public override float GetHeight(float width)
+            {
+                return EditorGUIUtility.singleLineHeight;
+            }
+
+            public override void OnGUI(Rect position)
+            {
+                var value = _propertyValue.Value;
+
+                EditorGUI.BeginChangeCheck();
+
+                var allowSceneObjects = _propertyValue.Property.PropertyTree.TargetObjects[0] is var targetObject &&
+                                        targetObject != null && !EditorUtility.IsPersistent(targetObject);
+
+                value = (T) EditorGUI.ObjectField(position, _propertyValue.Property.DisplayNameContent, value,
+                    _propertyValue.Property.FieldType, allowSceneObjects);
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    _propertyValue.Value = value;
+                }
+            }
+        }
+    }
+}
