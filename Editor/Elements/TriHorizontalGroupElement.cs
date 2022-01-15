1fc608985bab7cc69e454491449497cb449aa6f7
VladV <vanifatov.vlad@yandex.ru>
2022-01-15 18:06:45 +0300

    Move HorizontalGroup to core package

---
+++ Editor/Elements/TriHorizontalGroupElement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace TriInspector.Elements
+{
+    public class TriHorizontalGroupElement : TriPropertyCollectionBaseElement
+    {
+        public override float GetHeight(float width)
+        {
+            if (ChildrenCount == 0)
+            {
+                return 0f;
+            }
+
+            var height = 0f;
+
+            for (var i = 0; i < ChildrenCount; i++)
+            {
+                var child = GetChild(i);
+                var childWidth = width / ChildrenCount;
+                var childHeight = child.GetHeight(childWidth);
+
+                height = Mathf.Max(height, childHeight);
+            }
+
+            return height;
+        }
+
+        public override void OnGUI(Rect position)
+        {
+            if (ChildrenCount == 0)
+            {
+                return;
+            }
+
+            var childWidth = position.width / ChildrenCount;
+
+            for (var i = 0; i < ChildrenCount; i++)
+            {
+                var child = GetChild(i);
+                var childRect = new Rect(position)
+                {
+                    width = childWidth,
+                    x = position.x + i * childWidth,
+                };
+
+                child.OnGUI(childRect);
+            }
+        }
+    }
+}
