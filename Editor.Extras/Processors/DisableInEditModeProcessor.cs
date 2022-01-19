b44835a67949f31d41607d1c67168d4850289a6a
VladV <vanifatov.vlad@yandex.ru>
2022-01-19 13:07:09 +0300

    Add Show/Disable In Edit Mode attributes

---
+++ Editor.Extras/Processors/DisableInEditModeProcessor.cs
@@ -0,0 +1,25 @@
+﻿using TriInspector.Processors;
+using TriInspector;
+using UnityEngine;
+
+[assembly: RegisterTriPropertyDisableProcessor(typeof(DisableInEditModeProcessor))]
+[assembly: RegisterTriPropertyDisableProcessor(typeof(EnableInEditModeProcessor))]
+
+namespace TriInspector.Processors
+{
+    public class DisableInEditModeProcessor : TriPropertyDisableProcessor<DisableInEditModeAttribute>
+    {
+        public override bool IsDisabled(TriProperty property)
+        {
+            return !Application.isPlaying;
+        }
+    }
+
+    public class EnableInEditModeProcessor : TriPropertyDisableProcessor<EnableInEditModeAttribute>
+    {
+        public override bool IsDisabled(TriProperty property)
+        {
+            return Application.isPlaying;
+        }
+    }
+}
---
+++ Editor.Extras/Processors/HIdeInEditModeProcessor.cs
@@ -0,0 +1,25 @@
+﻿using TriInspector.Processors;
+using TriInspector;
+using UnityEngine;
+
+[assembly: RegisterTriPropertyHideProcessor(typeof(HideInEditModeProcessor))]
+[assembly: RegisterTriPropertyHideProcessor(typeof(ShowInEditModeProcessor))]
+
+namespace TriInspector.Processors
+{
+    public class HideInEditModeProcessor : TriPropertyHideProcessor<HideInEditModeAttribute>
+    {
+        public override bool IsHidden(TriProperty property)
+        {
+            return !Application.isPlaying;
+        }
+    }
+
+    public class ShowInEditModeProcessor : TriPropertyHideProcessor<ShowInEditModeAttribute>
+    {
+        public override bool IsHidden(TriProperty property)
+        {
+            return Application.isPlaying;
+        }
+    }
+}
