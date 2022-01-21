5c40f065cf73e61d4a61690cf4fd512f498f92c4
VladV <vanifatov.vlad@yandex.ru>
2022-01-21 18:15:05 +0300

    Delete TriMonoBehaviour and TriScriptableObject

--- Editor/Editors/MonoBehaviourEditor.cs
+++ Editor/Editors/MonoBehaviourEditor.cs
@@ -3,11 +3,9 @@ using UnityEngine;
 
 namespace TriInspector.Editors
 {
-#if TRI_INSPECTOR
     [CanEditMultipleObjects]
     [CustomEditor(typeof(MonoBehaviour), true, isFallback = true)]
     internal sealed class MonoBehaviourEditor : TriEditor
     {
     }
-#endif
 }
--- Editor/Editors/ScriptableObjectEditor.cs
+++ Editor/Editors/ScriptableObjectEditor.cs
@@ -3,11 +3,9 @@ using UnityEngine;
 
 namespace TriInspector.Editors
 {
-#if TRI_INSPECTOR
     [CanEditMultipleObjects]
     [CustomEditor(typeof(ScriptableObject), true, isFallback = true)]
     internal sealed class ScriptableObjectEditor : TriEditor
     {
     }
-#endif
 }
