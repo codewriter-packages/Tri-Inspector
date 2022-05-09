7c1e995bdf80f7e709acccf88b9e8d88c42756c1
VladV <vanifatov.vlad@yandex.ru>
2022-05-09 15:06:51 +0300

    Add method resolvers

---
+++ Editor/Resolvers/ErrorMethodResolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TriInspector.Resolvers
+{
+    internal class ErrorMethodResolver<TReturnType> : MethodResolver<TReturnType>
+    {
+        private readonly string _method;
+
+        public ErrorMethodResolver(TriPropertyDefinition propertyDefinition, string method)
+        {
+            _method = method;
+        }
+
+        public override bool TryGetErrorString(out string error)
+        {
+            error = $"Method '{_method}' not exists or has wrong signature";
+            return true;
+        }
+
+        public override TReturnType InvokeForTarget(TriProperty property, int targetIndex)
+        {
+            throw new InvalidOperationException("Method not exists");
+        }
+    }
+}
---
+++ Editor/Resolvers/InstanceMethodResolver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection;
+
+namespace TriInspector.Resolvers
+{
+    internal class InstanceMethodResolver<TReturnType> : MethodResolver<TReturnType>
+    {
+        private readonly MethodInfo _methodInfo;
+
+        public static bool TryResolve(TriPropertyDefinition propertyDefinition, string method,
+            out MethodResolver<TReturnType> resolver)
+        {
+            var parentType = propertyDefinition.MemberInfo.DeclaringType;
+            if (parentType == null)
+            {
+                resolver = null;
+                return false;
+            }
+
+            const BindingFlags flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+
+            foreach (var methodInfo in parentType.GetMethods(flags))
+            {
+                if (methodInfo.Name == method &&
+                    methodInfo.ReturnType == typeof(TReturnType) &&
+                    methodInfo.GetParameters() is var parameterInfos &&
+                    parameterInfos.Length == 0)
+                {
+                    resolver = new InstanceMethodResolver<TReturnType>(methodInfo);
+                    return true;
+                }
+            }
+
+            resolver = null;
+            return false;
+        }
+
+        private InstanceMethodResolver(MethodInfo methodInfo)
+        {
+            _methodInfo = methodInfo;
+        }
+
+        public override bool TryGetErrorString(out string error)
+        {
+            error = "";
+            return false;
+        }
+
+        public override TReturnType InvokeForTarget(TriProperty property, int targetIndex)
+        {
+            var parentValue = property.Parent.GetValue(targetIndex);
+
+            return (TReturnType) _methodInfo.Invoke(parentValue, Array.Empty<object>());
+        }
+    }
+}
