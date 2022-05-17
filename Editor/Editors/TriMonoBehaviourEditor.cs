36a78379c495e16e8ee47d932d11f2053ac5bf89
VladV <vanifatov.vlad@yandex.ru>
2022-05-17 13:16:24 +0300

    Add TriMonoBehaviour and TriStriptableObject

---
+++ Editor/Editors/TriMonoBehaviourEditor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+
+namespace TriInspector.Editors
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(TriMonoBehaviour), true, isFallback = true)]
+    internal sealed class TriMonoBehaviourEditor : TriEditor
+    {
+        
+    }
+}
---
+++ Runtime/TriMonoBehaviour.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace TriInspector
+{
+    public abstract class TriMonoBehaviour : MonoBehaviour
+    {
+    }
+}
