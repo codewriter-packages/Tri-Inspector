a17cfcf39d19fc7f8e23b4725a555f62c0dae2c8
VladV <vanifatov.vlad@yandex.ru>
2022-07-14 18:34:09 +0300

    Move TriDropdownElement to core

--- Editor.Extras/Drawers/DropdownDrawer.cs
+++ Editor.Extras/Drawers/DropdownDrawer.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using TriInspector;
 using TriInspector.Drawers;
+using TriInspector.Elements;
 using TriInspector.Resolvers;
-using UnityEditor;
-using UnityEngine;
 
 [assembly: RegisterTriAttributeDrawer(typeof(DropdownDrawer<>), TriDrawerOrder.Decorator)]
 
@@ -36,7 +34,7 @@ namespace TriInspector.Drawers
 
         public override TriElement CreateElement(TriProperty property, TriElement next)
         {
-            return new DropdownElement(property, GetDropdownItems);
+            return new TriDropdownElement(property, GetDropdownItems);
         }
 
         private IEnumerable<ITriDropdownItem> GetDropdownItems(TriProperty property)
@@ -59,80 +57,6 @@ namespace TriInspector.Drawers
                 {
                     yield return value;
                 }
-            }
-        }
-
-        private class DropdownElement : TriElement
-        {
-            private readonly TriProperty _property;
-            private readonly Func<TriProperty, IEnumerable<ITriDropdownItem>> _valuesGetter;
-
-            private string _currentText;
-
-            public DropdownElement(TriProperty property, Func<TriProperty, IEnumerable<ITriDropdownItem>> valuesGetter)
-            {
-                _property = property;
-                _valuesGetter = valuesGetter;
-            }
-
-            protected override void OnAttachToPanel()
-            {
-                base.OnAttachToPanel();
-
-                _property.ValueChanged += OnValueChanged;
-
-                RefreshCurrentText();
-            }
-
-            protected override void OnDetachFromPanel()
-            {
-                _property.ValueChanged -= OnValueChanged;
-
-                base.OnDetachFromPanel();
-            }
-
-            public override float GetHeight(float width)
-            {
-                return EditorGUIUtility.singleLineHeight;
-            }
-
-            public override void OnGUI(Rect position)
-            {
-                var controlId = GUIUtility.GetControlID(FocusType.Passive);
-                position = EditorGUI.PrefixLabel(position, controlId, _property.DisplayNameContent);
-
-                if (GUI.Button(position, _currentText, EditorStyles.popup))
-                {
-                    ShowDropdown(position);
-                }
-            }
-
-            private void OnValueChanged(TriProperty property)
-            {
-                RefreshCurrentText();
-            }
-
-            private void RefreshCurrentText()
-            {
-                var items = _valuesGetter.Invoke(_property);
-
-                _currentText = items
-                    .FirstOrDefault(it => _property.Comparer.Equals(it.Value, _property.Value))
-                    ?.Text ?? "";
-            }
-
-            private void ShowDropdown(Rect position)
-            {
-                var items = _valuesGetter.Invoke(_property);
-                var menu = new GenericMenu();
-
-                foreach (var item in items)
-                {
-                    var isOn = _property.Comparer.Equals(item.Value, _property.Value);
-                    menu.AddItem(new GUIContent(item.Text), isOn, _property.SetValue, item.Value);
-                }
-
-                menu.DropDown(position);
             }
         }
     }
