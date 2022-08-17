4fa15f6e1c41b4d69128d8595ca9bc370d2d3237
VladV <vanifatov.vlad@yandex.ru>
2022-08-17 20:16:23 +0300

    Add Type processors

--- Editor/TriTypeDefinition.cs
+++ Editor/TriTypeDefinition.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using TriInspector.Utilities;
 
 namespace TriInspector
@@ -11,53 +9,12 @@ namespace TriInspector
         private static readonly Dictionary<Type, TriTypeDefinition> Cache =
             new Dictionary<Type, TriTypeDefinition>();
 
-        private TriTypeDefinition(Type type)
+        private TriTypeDefinition(IReadOnlyList<TriPropertyDefinition> properties)
         {
-            var fieldsOffset = 1;
-            var fields = TriReflectionUtilities
-                .GetAllInstanceFieldsInDeclarationOrder(type)
-                .Where(IsSerialized)
-                .Select((it, ind) => new TriPropertyDefinition(ind + fieldsOffset, it))
-                .ToList();
-
-            var propertiesOffset = fieldsOffset + fields.Count;
-            var properties = TriReflectionUtilities
-                .GetAllInstancePropertiesInDeclarationOrder(type)
-                .Where(IsSerialized)
-                .Select((it, ind) => new TriPropertyDefinition(ind + propertiesOffset, it))
-                .ToList();
-
-            var methodsOffset = propertiesOffset + properties.Count;
-            var methods = TriReflectionUtilities
-                .GetAllInstanceMethodsInDeclarationOrder(type)
-                .Where(IsSerialized)
-                .Select((it, ind) => new TriPropertyDefinition(ind + methodsOffset, it))
-                .ToList();
-
-            Properties = Enumerable.Empty<TriPropertyDefinition>()
-                .Concat(fields)
-                .Concat(properties)
-                .Concat(methods)
-                .OrderBy(it => it.Order)
-                .ToList();
+            Properties = properties;
         }
 
         public IReadOnlyList<TriPropertyDefinition> Properties { get; }
-
-        private static bool IsSerialized(FieldInfo fieldInfo)
-        {
-            return TriUnitySerializationUtilities.IsSerializableByUnity(fieldInfo);
-        }
-
-        private static bool IsSerialized(PropertyInfo propertyInfo)
-        {
-            return propertyInfo.GetCustomAttribute<ShowInInspector>() != null;
-        }
-
-        private static bool IsSerialized(MethodInfo methodInfo)
-        {
-            return methodInfo.GetCustomAttribute<ButtonAttribute>() != null;
-        }
 
         public static TriTypeDefinition GetCached(Type type)
         {
@@ -66,7 +23,15 @@ namespace TriInspector
                 return definition;
             }
 
-            return Cache[type] = new TriTypeDefinition(type);
+            var processors = TriDrawersUtilities.AllTypeProcessors;
+            var properties = new List<TriPropertyDefinition>();
+
+            foreach (var processor in processors)
+            {
+                processor.ProcessType(type, properties);
+            }
+
+            return Cache[type] = new TriTypeDefinition(properties);
         }
     }
 }
