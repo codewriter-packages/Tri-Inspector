9d096fac5d6986dfc51c45a0bb63139b6b909ba9
VladV <vanifatov.vlad@yandex.ru>
2022-12-09 19:59:33 +0400

    Allows ShowInInspector usage on fields

--- Runtime/Attributes/ShowInInspector.cs
+++ Runtime/Attributes/ShowInInspector.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 
 namespace TriInspector
 {
-    [AttributeUsage(AttributeTargets.Property)]
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     [Conditional("UNITY_EDITOR")]
     public class ShowInInspector : Attribute
     {
