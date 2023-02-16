81f532682c492e07a90f8919acfb6309d34dee4f
VladV <vanifatov.vlad@yandex.ru>
2023-02-16 12:11:03 +0400

    Fix RectOffset drawn not correctly

---
+++ Editor/TypeProcessors/TriRectOffsetTypeProcessor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using TriInspector;
+using TriInspector.TypeProcessors;
+using UnityEngine;
+
+[assembly: RegisterTriTypeProcessor(typeof(TriRectOffsetTypeProcessor), 1)]
+
+namespace TriInspector.TypeProcessors
+{
+    public class TriRectOffsetTypeProcessor : TriTypeProcessor
+    {
+        private static readonly string[] DrawnProperties = new[]
+        {
+            "left",
+            "right",
+            "top",
+            "bottom",
+        };
+
+        public override void ProcessType(Type type, List<TriPropertyDefinition> properties)
+        {
+            if (type != typeof(RectOffset))
+            {
+                return;
+            }
+
+            for (var i = 0; i < DrawnProperties.Length; i++)
+            {
+                var propertyName = DrawnProperties[i];
+                var propertyInfo = type.GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Public);
+                var propertyDef = TriPropertyDefinition.CreateForPropertyInfo(i, propertyInfo);
+
+                properties.Add(propertyDef);
+            }
+        }
+    }
+}
