3d8d7b429e9bd9a0a508b3e239a82c4564108ccb
VladV <vanifatov.vlad@yandex.ru>
2023-03-12 11:27:02 +0400

    Add DisplayAsString attrribute

---
+++ Editor.Extras/Drawers/DisplayAsStringDrawer.cs
@@ -0,0 +1,27 @@
+﻿using TriInspector;
+using TriInspector.Drawers;
+using UnityEditor;
+using UnityEngine;
+
+[assembly: RegisterTriAttributeDrawer(typeof(DisplayAsStringDrawer), TriDrawerOrder.Decorator, ApplyOnArrayElement = true)]
+
+namespace TriInspector.Drawers
+{
+    public class DisplayAsStringDrawer : TriAttributeDrawer<DisplayAsStringAttribute>
+    {
+        public override float GetHeight(float width, TriProperty property, TriElement next)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        public override void OnGUI(Rect position, TriProperty property, TriElement next)
+        {
+            var value = property.Value;
+            var text = value != null ? value.ToString() : "Null";
+
+            var controlId = GUIUtility.GetControlID(FocusType.Passive);
+            position = EditorGUI.PrefixLabel(position, controlId, property.DisplayNameContent);
+            GUI.Label(position, text);
+        }
+    }
+}
---
+++ Editor.Samples/Decorators/Decorators_DisplayAsStringSample.cs
@@ -0,0 +1,11 @@
+﻿using TriInspector;
+using UnityEngine;
+
+public class Decorators_DisplayAsStringSample : ScriptableObject
+{
+    [DisplayAsString]
+    public string hello = "world";
+
+    [DisplayAsString, HideLabel]
+    public Texture2D texture;
+}
