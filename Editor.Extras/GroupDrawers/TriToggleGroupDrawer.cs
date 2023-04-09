e40ad952f45c6f23bfbc94c80c5b1571d5c989d1
HoSHIZA <58137717+HoSHIZA@users.noreply.github.com>
2023-04-09 19:51:22 +0300

    Added ToggleGroup attribute (#100)

---
+++ Editor.Extras/GroupDrawers/TriToggleGroupDrawer.cs
@@ -0,0 +1,21 @@
+﻿using TriInspector;
+using TriInspector.Elements;
+using TriInspector.GroupDrawers;
+
+[assembly: RegisterTriGroupDrawer(typeof(TriToggleGroupDrawer))]
+
+namespace TriInspector.GroupDrawers
+{
+    public class TriToggleGroupDrawer : TriGroupDrawer<DeclareToggleGroupAttribute>
+    {
+        public override TriPropertyCollectionBaseElement CreateElement(DeclareToggleGroupAttribute attribute)
+        {
+            return new TriBoxGroupElement(new TriBoxGroupElement.Props
+            {
+                title = attribute.Title,
+                titleMode = TriBoxGroupElement.TitleMode.Toggle,
+                expandedByDefault = attribute.Collapsible,
+            });
+        }
+    }
+}
---
+++ Editor.Samples/Groups/Groups_ToggleGroupSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TriInspector;
+using UnityEngine;
+
+[DeclareToggleGroup("My Toggle")]
+[DeclareToggleGroup("My Non Collapsible Toggle", Collapsible = false)]
+[DeclareToggleGroup("boxed_toggle_struct", Title = "Toggle Struct")]
+public class Groups_ToggleGroupSample : ScriptableObject
+{
+    [Group("My Toggle")] public bool aEnabled = true;
+    [Group("My Toggle")] public string b;
+    [Group("My Toggle")] public bool c;
+
+    [Group("My Non Collapsible Toggle")] public bool dEnabled;
+    [Group("My Non Collapsible Toggle")] public bool e;
+    [Group("My Non Collapsible Toggle")] public Vector3 f;
+    
+    [Group("boxed_toggle_struct"), InlineProperty, HideLabel]
+    public MyStruct boxedStruct;
+
+    public MyStruct defaultStruct;
+
+    [Serializable]
+    public struct MyStruct
+    {
+        public bool enabled;
+        public int a;
+        public float b;
+    }
+}
