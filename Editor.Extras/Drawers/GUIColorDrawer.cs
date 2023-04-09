314a640c0ec0257a4029fbf18a3ea0028a832b35
HoSHIZA <needahentai@gmail.com>
2023-04-09 21:54:12 +0300

    Extend the GUIColorAttribute functionality

--- Editor.Extras/Drawers/GUIColorDrawer.cs
+++ Editor.Extras/Drawers/GUIColorDrawer.cs
@@ -1,5 +1,6 @@
 ﻿using TriInspector;
 using TriInspector.Drawers;
+using TriInspector.Resolvers;
 using UnityEngine;
 
 [assembly: RegisterTriAttributeDrawer(typeof(GUIColorDrawer), TriDrawerOrder.Decorator)]
@@ -11,13 +12,24 @@ namespace TriInspector.Drawers
         public override void OnGUI(Rect position, TriProperty property, TriElement next)
         {
             var oldColor = GUI.color;
-            var newColor = new Color(Attribute.R, Attribute.G, Attribute.B, Attribute.A);
-
+            var newColor = Color.white;
+            
+            if (string.IsNullOrEmpty(Attribute.GetColor))
+            {
+                newColor = Attribute.Color;
+            }
+            else
+            {
+                var colorResolver = ValueResolver.Resolve<Color>(property.Definition, Attribute.GetColor ?? "");
+                
+                newColor = colorResolver.GetValue(property, Color.white);
+            }
+            
             GUI.color = newColor;
             GUI.contentColor = newColor;
-
+            
             next.OnGUI(position);
-
+            
             GUI.color = oldColor;
             GUI.contentColor = oldColor;
         }
