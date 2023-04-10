7412caa665285903938d867cd521fdd7339fe9f3
HoSHIZA <needahentai@gmail.com>
2023-04-10 13:51:03 +0300

    Checking a field for the right type has been optimized

--- Editor.Extras/Validators/RegexValidator.cs
+++ Editor.Extras/Validators/RegexValidator.cs
@@ -18,7 +18,12 @@ namespace TriInspector.Validators
         public override TriExtensionInitializationResult Initialize(TriPropertyDefinition propertyDefinition)
         {
             base.Initialize(propertyDefinition);
-
+            
+            if (propertyDefinition.FieldType != typeof(string))
+            {
+                return "Scene attribute can only be used on field of type string";
+            }
+            
             if (Attribute.DynamicExpression && Attribute.Expression.StartsWith("$"))
             {
                 _resolver = ValueResolver.ResolveString(propertyDefinition, Attribute.Expression ?? "");
@@ -34,60 +39,55 @@ namespace TriInspector.Validators
 
         public override TriValidationResult Validate(TriProperty property)
         {
-            if (property.FieldType == typeof(string))
+            var value = property.Value.ToString();
+            var expression = Attribute.Expression;
+
+            if (string.IsNullOrEmpty(Attribute.Expression))
             {
-                var value = property.Value.ToString();
-                var expression = Attribute.Expression;
-                
-                if (string.IsNullOrEmpty(Attribute.Expression))
-                {
-                    return TriValidationResult.Valid;
-                }
-                
-                if (Attribute.DynamicExpression && expression.StartsWith("$"))
-                {
-                    if (_resolver.TryGetErrorString(out var error))
-                    {
-                        return TriValidationResult.Error(error);
-                    }
-                    
-                    expression = _resolver.GetValue(property);
-                }
-                
-                try
-                {
-                    _regex = new Regex(expression);
-                }
-                catch
-                {
-                    return TriValidationResult.Error("The expression is invalid");
-                }
-                
-                if (_regex.IsMatch(value))
-                {
-                    return TriValidationResult.Valid;
-                }
-                
-                _errorStringBuilder.Append("The value does not match the expression");
-                
-                if (Attribute.PreviewExpression)
-                {
-                    _errorStringBuilder.Append($"\nExample: {Attribute.Example}");
-                }
-                
-                if (!string.IsNullOrEmpty(Attribute.Example))
-                {
-                    _errorStringBuilder.Append($"\nExpression: @\"{expression}\"");
-                }
-                
-                var errorString = _errorStringBuilder.ToString();
-                
-                _errorStringBuilder.Clear();
-                
-                return TriValidationResult.Error(errorString);
+                return TriValidationResult.Valid;
             }
-            
-            return TriValidationResult.Error("RegexAttribute only valid on String");
+
+            if (Attribute.DynamicExpression && expression.StartsWith("$"))
+            {
+                if (_resolver.TryGetErrorString(out var error))
+                {
+                    return TriValidationResult.Error(error);
+                }
+
+                expression = _resolver.GetValue(property);
+            }
+
+            try
+            {
+                _regex = new Regex(expression);
+            }
+            catch
+            {
+                return TriValidationResult.Error("The expression is invalid");
+            }
+
+            if (_regex.IsMatch(value))
+            {
+                return TriValidationResult.Valid;
+            }
+
+            _errorStringBuilder.Append("The value does not match the expression");
+
+            if (Attribute.PreviewExpression)
+            {
+                _errorStringBuilder.Append($"\nExample: {Attribute.Example}");
+            }
+
+            if (!string.IsNullOrEmpty(Attribute.Example))
+            {
+                _errorStringBuilder.Append($"\nExpression: @\"{expression}\"");
+            }
+
+            var errorString = _errorStringBuilder.ToString();
+
+            _errorStringBuilder.Clear();
+
+            return TriValidationResult.Error(errorString);
         }
     }
 }
