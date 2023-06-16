5dc71785afb8badbf539be1883170b096c763c5c
VladV <vanifatov.vlad@yandex.ru>
2023-06-16 17:25:07 +0400

    Add inline fix for validation messages

--- Editor.Samples/Validators/Validators_ValidateInputSample.cs
+++ Editor.Samples/Validators/Validators_ValidateInputSample.cs
@@ -6,10 +6,24 @@ public class Validators_ValidateInputSample : ScriptableObject
     [ValidateInput(nameof(ValidateTexture))]
     public Texture tex;
 
+    [ValidateInput(nameof(ValidateNumber))]
+    public int number;
+
     private TriValidationResult ValidateTexture()
     {
         if (tex == null) return TriValidationResult.Error("Tex is null");
         if (!tex.isReadable) return TriValidationResult.Warning("Tex must be readable");
         return TriValidationResult.Valid;
+    }
+
+    private TriValidationResult ValidateNumber()
+    {
+        if (number == 1)
+        {
+            return TriValidationResult.Valid;
+        }
+
+        return TriValidationResult.Error("Number must be equal 1")
+            .WithFix(() => number = 1, "Set to 1");
     }
 }
