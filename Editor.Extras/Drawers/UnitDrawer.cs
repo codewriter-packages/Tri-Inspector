5532d46cbca06c51233cdd34ec47c98b2572c474
Kinggrass <dennis.grass@gmx.de>
2025-02-07 22:59:49 +0100

    Add unit attribute and drawer functionality
    - Introduced a new UnitAttribute for specifying measurement units.
    - Created a UnitDrawer to display the unit next to input fields in the editor.
    - Added sample script showcasing various units like meters, kilograms, and seconds.

---
+++ Editor.Extras/Drawers/UnitDrawer.cs
@@ -0,0 +1,45 @@
+﻿using TriInspector;
+using TriInspector.Drawers;
+using UnityEditor;
+using UnityEngine;
+
+[assembly: RegisterTriAttributeDrawer(typeof(UnitDrawer), TriDrawerOrder.Decorator)]
+
+namespace TriInspector.Drawers
+{
+    public class UnitDrawer : TriAttributeDrawer<UnitAttribute>
+    {
+        /// <summary>
+        /// Defines the padding to the right of the unit label towards the editable input field
+        /// </summary>
+        private const int paddingRight = 5;
+        private GUIStyle unitStyle;
+        private GUIContent content;
+
+        public override void OnGUI(Rect position, TriProperty property, TriElement next)
+        {
+            if(unitStyle == null){
+                unitStyle = new(EditorStyles.label);
+            }
+
+            if(content == null){
+                content = new(Attribute.unitToDisplay);
+            }
+
+            Vector2 size = unitStyle.CalcSize(content);
+
+            var unitRect = new Rect(position.xMax - size.x - paddingRight, position.y, size.x, position.height);
+
+            // Render the editable input field
+            next.OnGUI(position);
+
+            //Change color to grey
+            var tmpColor = GUI.color;
+            GUI.color = Color.grey;
+            // Render the unit as a suffix in the unitRect
+            EditorGUI.LabelField(unitRect, content);
+            // Restore the original color
+            GUI.color = tmpColor;
+        }
+    }
+}
