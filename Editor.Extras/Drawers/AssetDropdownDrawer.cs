abefc401d70eb6933ef658eadde628f2a6abd22a
VladV <vanifatov.vlad@yandex.ru>
2025-10-18 13:29:12 +0300

    Added customizable AssetDropdown attribute

---
+++ Editor.Extras/Drawers/AssetDropdownDrawer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TriInspector;
+using TriInspector.Drawers;
+using TriInspector.Elements;
+using TriInspector.Utilities;
+using UnityEditor;
+using UnityEngine;
+
+[assembly: RegisterTriAttributeDrawer(typeof(AssetDropdownDrawer<>), TriDrawerOrder.Decorator,
+    ApplyOnArrayElement = true)]
+
+namespace TriInspector.Drawers
+{
+    public class AssetDropdownDrawer<T> : TriAttributeDrawer<AssetDropdownAttribute>
+    {
+        private bool showNoneElement;
+
+        public override TriExtensionInitializationResult Initialize(TriPropertyDefinition propertyDefinition)
+        {
+            var type = propertyDefinition.FieldType;
+            if (!typeof(Object).IsAssignableFrom(type))
+            {
+                return "AssetDropdown attribute can only be used on field with UnityEngine.Object type";
+            }
+
+            showNoneElement = !propertyDefinition.Attributes.TryGet<RequiredAttribute>(out _);
+
+            return base.Initialize(propertyDefinition);
+        }
+
+        public override TriElement CreateElement(TriProperty property, TriElement next)
+        {
+            var dropdownElement = new TriDropdownElement(property, EnumerateAssets);
+
+            if (Attribute.HideNextDrawer)
+            {
+                return dropdownElement;
+            }
+
+            var line = new TriHorizontalGroupElement();
+            line.AddChild(dropdownElement);
+            line.AddChild(base.CreateElement(property, next));
+            return line;
+        }
+
+        private IEnumerable<ITriDropdownItem> EnumerateAssets(TriProperty property)
+        {
+            var assets = AssetDatabase.FindAssets(Attribute.Filter, Attribute.SearchInFolders)
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<Object>)
+                .Where(asset => asset is T)
+                .Select(asset => (ITriDropdownItem) new TriDropdownItem<T>
+                {
+                    Text = Attribute.GetDisplayName(asset),
+                    Value = (T) (object) asset,
+                });
+
+            if (showNoneElement)
+            {
+                assets = assets.Prepend(new TriDropdownItem<T> {Text = "None", Value = default,});
+            }
+
+            return assets;
+        }
+    }
+}
---
+++ Editor.Samples/Decorators/Decorators_AssetDropdownSample.cs
@@ -0,0 +1,33 @@
+﻿using TriInspector;
+using UnityEngine;
+
+public class Decorators_AssetDropdownSample : ScriptableObject
+{
+    // The full syntax of filters can be found here:
+    // https://docs.unity3d.com/Documentation/ScriptReference/AssetDatabase.FindAssets.html
+
+    // Dropdown list of textures located in the `Assets/Textures/Character` folder
+    [AssetDropdown("t: Texture2D", searchInFolders: new[] {"Assets/Textures/Character"})]
+    public Texture2D character;
+
+    // Custom dropdown
+    [Required, BuildingDropdown]
+    public BuildingConfig myBuilding;
+}
+
+// Custom dropdown attribute of assets with `BuildingConfig` type
+public class BuildingDropdownAttribute : AssetDropdownAttribute
+{
+    public BuildingDropdownAttribute() : base($"t: {nameof(BuildingConfig)}") { }
+
+    // Overrides default names of dropdown elements
+    public override string GetDisplayName(Object asset) => asset is BuildingConfig buildingConfig
+        ? buildingConfig.niceName
+        : base.GetDisplayName(asset);
+}
+
+// Demo example configuration file
+public class BuildingConfig : ScriptableObject
+{
+    public string niceName;
+}
