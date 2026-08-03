d75c4dac2e449bc19b5313ce741bd496fca7c123
yungs <yungshing@outlook.com>
2026-08-03 17:32:08 +0800

    feat: InlineButton (#236)

---
+++ Runtime/Attributes/Buttons/InlineButtonAttribute.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Diagnostics;
+
+namespace TriInspector
+{
+    [AttributeUsage(AttributeTargets.Field|AttributeTargets.Property)]
+    [Conditional("UNITY_EDITOR")]
+    public class InlineButtonAttribute : Attribute
+    {
+        public InlineButtonAttribute(string name)
+        {
+            Name = name;
+        }
+        public InlineButtonAttribute(string label,string name):this(name)
+        {
+            ButtonLabel = label;
+        }
+
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Optional custom label for the button. If not set, the method name is used.
+        /// </summary>
+        public string ButtonLabel { get; set; }
+
+        /// <summary>
+        /// Width of the inline button in pixels. Default is 60.
+        /// </summary>
+        public float ButtonWidth { get; set; }
+    }
+}
