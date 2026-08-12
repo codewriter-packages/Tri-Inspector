c0867e2854dca0f2b332f7107271060b252c393f
VladV <vanifatov.vlad@yandex.ru>
2026-08-12 21:51:02 +0400

    Add Header attribute fallback drawer for ShowInInspector properties

---
+++ Editor.Extras/Drawers/HeaderDrawer.cs
@@ -0,0 +1,39 @@
+﻿using TriInspector;
+using TriInspector.Drawers;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[assembly: RegisterTriAttributeDrawer(typeof(HeaderDrawer), 8900)]
+
+namespace TriInspector.Drawers
+{
+    public class HeaderDrawer : TriAttributeDrawer<HeaderAttribute>
+    {
+        public override VisualElement CreateVisualElement(TriProperty property, VisualElement next)
+        {
+            if (property.TryGetSerializedProperty(out _))
+            {
+                return next;
+            }
+
+            return new TriHeader(next, Attribute);
+        }
+
+        private class TriHeader : VisualElement
+        {
+            public TriHeader(VisualElement next, HeaderAttribute attribute)
+            {
+                var title = new Label(attribute.header)
+                {
+                    style =
+                    {
+                        marginTop = 13,
+                        unityFontStyleAndWeight = FontStyle.Bold,
+                    },
+                };
+                Add(title);
+                Add(next);
+            }
+        }
+    }
+}
