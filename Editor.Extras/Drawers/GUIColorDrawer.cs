6d4038fd847fc44432a60b314cb891349931524b
VladV <vanifatov.vlad@yandex.ru>
2026-09-10 14:17:19 +0400

    Fix GUIColor drawer colors

--- Editor.Extras/Drawers/GUIColorDrawer.cs
+++ Editor.Extras/Drawers/GUIColorDrawer.cs
@@ -1,5 +1,6 @@
 using JetBrains.Annotations;
 using TriInspector.Resolvers;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -29,7 +30,33 @@ namespace TriInspector.Drawers
         {
             void SetColor(Color value)
             {
-                next.style.backgroundColor = value;
+                Color.RGBToHSV(value, out var h, out var s, out var v);
+
+                var buttonColor = EditorGUIUtility.isProSkin
+                    ? Color.HSVToRGB(h, s * 0.7f, v * 0.4f)
+                    : Color.HSVToRGB(h, s * 1.0f, v * 0.9f);
+
+                var inputFieldColor = EditorGUIUtility.isProSkin
+                    ? Color.HSVToRGB(h, s * 0.7f, v * 0.165f)
+                    : Color.HSVToRGB(h, s * 1.0f, v * 0.9f);
+
+                var textColor = EditorGUIUtility.isProSkin
+                    ? Color.HSVToRGB(h, s * 1.0f, v * 1.0f)
+                    : Color.HSVToRGB(h, s * 1.0f, v * 0.2f);
+
+                next.Query<Label>()
+                    .ForEach(label => label.style.color = textColor);
+                next.Query(className: "unity-base-field__input")
+                    .ForEach(input => input.style.color = textColor);
+                next.Query(className: "unity-base-text-field__input")
+                    .ForEach(input => input.style.backgroundColor = inputFieldColor);
+                next.Query(className: "unity-toggle__checkmark")
+                    .ForEach(checkmark => checkmark.style.unityBackgroundImageTintColor = value);
+                next.Query<Button>().ForEach(button =>
+                {
+                    button.style.color = textColor;
+                    button.style.backgroundColor = buttonColor;
+                });
             }
 
             if (_colorResolver != null)
